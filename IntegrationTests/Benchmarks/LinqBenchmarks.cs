a80e3f070b65cedb837dc7975b50d4f287bb7e4d
Matthew Lewis <mlewis@singular.co.za>
2024-08-17 18:10:45 +0200

    Added a benhcmark to test the time complexity of a Linq statement

---
+++ IntegrationTests/Benchmarks/LinqBenchmarks.cs
@@ -0,0 +1,33 @@
+﻿namespace IntegrationTests;
+
+using System.Linq;
+
+public class LinqBenchmarks
+{
+  [Fact]
+  public void LinqStatementTimeComplexityBenchmarkTest()
+  {
+    // Arrange
+    var iterationCount = 0;
+    var collection = new List<int> { 1, 2, 3, 4, 5 };
+    var expectedIterationComplexity = collection.Count;
+
+    var monitoredCollection = collection.Select(element =>
+    {
+      // The counter will increment each time an element is accessed
+      iterationCount++;
+      return element;
+    });
+
+    // Act
+    var result = monitoredCollection
+      .Where(x => x > 2)
+      .Where(x => x % 2 == 0)
+      .Where(x  => x > 3)
+      .ToList();
+
+    // Assert
+    result.Should().NotBeEmpty();
+    iterationCount.Should().Be(expectedIterationComplexity);
+  }
+}
